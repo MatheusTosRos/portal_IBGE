0dccac1befd4694160d5ebcac6731f997af1f7ba
MatheusTosRos <125379665+MatheusTosRos@users.noreply.github.com>
2024-06-13 15:45:34 +0000

    creating files

---
+++ index.js
@@ -0,0 +1,11 @@
+const dialog = document.getElementById('dialog')
+const botaoFiltro = document.getElementById('filtro')
+const aplicaBotao = document.getElementById('aplica');
+
+botaoFiltro.addEventListener("click", function () {
+    dialog.showModal();
+});
+
+aplicaBotao.addEventListener("click", function () {
+    dialog.close();
+});
