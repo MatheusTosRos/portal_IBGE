57165ba331a3a31594776e6bf203a3752ae818ca
MatheusTosRos <matheustosros@gmail.com>
2024-06-15 15:46:40 -0300

    update index.html

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const dialog = document.getElementById('dialog-filter')
+const botaoFiltro = document.getElementById('modal-button')
+const aplicaBotao = document.getElementById('aplicar')
+const submitBotao = document.getElementById("busca-input")
+
+document.addEventListener("keyprees", function(event) {
+    if (event.keyCode === "Enter") {
+        submitBotao.click();
+    }
+})
+
 document.addEventListener("DOMContentLoaded", function() {
-const dialog = document.getElementById('dialog')
-const botaoFiltro = document.getElementById('filtro')
-const aplicaBotao = document.getElementById('aplica');
 
 botaoFiltro.addEventListener("click", function () {
     dialog.showModal();
@@ -10,4 +18,4 @@ botaoFiltro.addEventListener("click", function () {
 aplicaBotao.addEventListener("click", function () {
     dialog.close();
 });
-}
+})
